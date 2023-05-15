3a05059dfe87c7c424edebe17e451b736e7df0c3
Arsh Kumar <85900453+arsh0007@users.noreply.github.com>
2023-05-15 22:09:22 +0530

    Create Day3Counter2.js

---
+++ Day3Counter2.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    a=i= init;
+    function increment()
+    {
+        return ++a
+    }
+    function decrement()
+    {
+        return --a
+    }
+    function reset()
+    {
+        return a=i
+    }
+    return {increment, decrement, reset}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */

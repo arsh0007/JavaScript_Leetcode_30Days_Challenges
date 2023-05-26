5b61e04af9ab3f083e69f8057154553b77cfd3b0
Arsh Kumar <85900453+arsh0007@users.noreply.github.com>
2023-05-26 08:52:11 +0530

    Create Day22FlatternDeeplyNestedArray.js
    Given a multi-dimensional array arr and a depth n, return a flattened version of that array.
    
    A multi-dimensional array is a recursive data structure that contains integers or other multi-dimensional arrays.
    
    A flattened array is a version of that array with some or all of the sub-arrays removed and replaced with the actual elements in that sub-array. This flattening operation should only be done if the current depth of nesting is less than n. The depth of the elements in the first array are considered to be 0.
    
    Please solve it without the built-in Array.flat method.
    
     
    
    Example 1:
    
    Input
    arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
    n = 0
    Output
    [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
    
    Explanation
    Passing a depth of n=0 will always result in the original array. This is because the smallest possible depth of a subarray (0) is not less than n=0. Thus, no subarray should be flattened. 
    Example 2:
    
    Input
    arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
    n = 1
    Output
    [1, 2, 3, 4, 5, 6, 7, 8, [9, 10, 11], 12, 13, 14, 15]
    
    Explanation
    The subarrays starting with 4, 7, and 13 are all flattened. This is because their depth of 0 is less than 1. However [9, 10, 11] remains unflattened because its depth is 1.
    Example 3:
    
    Input
    arr = [[1, 2, 3], [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
    n = 2
    Output
    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
    
    Explanation
    The maximum depth of any subarray is 1. Thus, all of them are flattened.

---
+++ Day22FlatternDeeplyNestedArray.js
@@ -0,0 +1,23 @@
+/**
+ * @param {any[]} arr
+ * @param {number} depth
+ * @return {any[]}
+ */
+var flat = function (arr, n) {
+    if(n==0) {
+        return arr;
+    }
+
+    let result = [];
+
+    for(let i=0; i<arr.length; i++) {
+        if(n>0 && Array.isArray(arr[i])) {
+            result.push(...flat(arr[i], n-1));
+        }
+
+        else {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
